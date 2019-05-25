798294d9ba3a4f3e1037e2068aa9b67983189936
Lucas Manzano Farias <lucasmfarias1@gmail.com>
2019-05-25 15:09:10 -0300

    configurar banda#show para mostrar albums e configurar select de generos para banda

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def explorar
+    @albums = Album.all
+    @bandas = Banda.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'buscar', to: 'busca#index'
+
   devise_for :users
   resources :albums
   resources :bandas
+
   root to: 'home#index'
 end
