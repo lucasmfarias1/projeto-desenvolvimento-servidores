3e1ad55f0fe3e9c6398125f342659e8feca83399
Lucas Manzano Farias <lucasmfarias1@gmail.com>
2019-05-25 15:07:29 -0300

    configurar validacao basica de Banda

--- app/models/banda.rb
+++ app/models/banda.rb
@@ -1,3 +1,7 @@
 class Banda < ApplicationRecord
-  has_many :albums
+  has_many :albums, dependent: :destroy
+
+  validates :nome, length: { in: 1..40 }
+  validates :ano, numericality: { only_integer: true, greater_than: 1877, less_than: 2020 }
+  validates :genero, inclusion: { in: ['Hardcore', 'Progressivo', 'Punk', 'Heavy Metal'], message: "%{value} não é um gênero válido" }
 end
