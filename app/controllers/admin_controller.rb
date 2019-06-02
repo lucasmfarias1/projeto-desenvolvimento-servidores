5f43b2bf82d91a416b134100dc51bf46ca9da921
Lucas Manzano Farias <lucasmfarias1@gmail.com>
2019-06-02 15:08:10 -0300

    ajustar redirecionamento de aprovar/reprovar banda/album em admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -11,26 +11,26 @@ class AdminController < ApplicationController
     @banda = Banda.find(params[:id])
     @banda.pendente = 0
     @banda.save
-    redirect_to admin_path, notice: "Você aprovou a banda #{@banda.nome} com sucesso"
+    redirect_to req_path, notice: "Você aprovou a banda #{@banda.nome} com sucesso"
   end
 
   def reprova_banda
     @banda = Banda.find(params[:id])
     @banda.destroy
-    redirect_to admin_path, notice: "Você reprovou a banda #{@banda.nome} com sucesso"
+    redirect_to req_path, notice: "Você reprovou a banda #{@banda.nome} com sucesso"
   end
 
   def aprova_album
     @album = Album.find(params[:id])
     @album.pendente = 0
     @album.save
-    redirect_to admin_path, notice: "Você aprovou o album #{@album.nome} com sucesso"
+    redirect_to req_path, notice: "Você aprovou o album #{@album.nome} com sucesso"
   end
 
   def reprova_album
     @album = Album.find(params[:id])
     @album.destroy
-    redirect_to admin_path, notice: "Você reprovou o album #{@album.nome} com sucesso"
+    redirect_to req_path, notice: "Você reprovou o album #{@album.nome} com sucesso"
   end
 
   private
